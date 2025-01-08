637398a7b790d22e6dd45e25b12b68ab05b13c95
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-01-08 10:02:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Doubly Linked List/introduction-to-doubly-linked-list.java
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        prev = null;
+    }
+}
+
+class GFG {
+    public static void printList(Node node) {
+        Node tmp = node;
+        int c1 = 0, c2 = 0;
+        if (tmp != null) {
+            while (tmp.next != null) {
+                c1++;
+                tmp = tmp.next;
+            }
+            while (tmp.prev != null) {
+                c2++;
+                tmp = tmp.prev;
+            }
+            if (c1 != c2) {
+                System.out.println("-1");
+                return;
+            }
+        }
+        while (tmp != null) {
+            System.out.print(tmp.data + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            Node ans = obj.constructDLL(arr);
+            printList(ans);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    Node constructDLL(int arr[]) {
+        // Code here
+        if (arr.length == 0) {
+            return null; // Return null for an empty linked list
+        }
+
+        // Create the head node of the linked list
+        Node head = new Node(arr[0]);
+        Node current = head;
+        // Node prev = null;
+
+        // Iterate through the rest of the array and create nodes
+        for (int i = 1; i < arr.length; i++) {
+            Node newnode =new Node(arr[i]);
+            current.next = newnode;
+            newnode.prev=current;
+       current=newnode;
+            
+            // Move to the next node
+        }
+
+        return head; 
+        
+    
+     
+    }
+}
