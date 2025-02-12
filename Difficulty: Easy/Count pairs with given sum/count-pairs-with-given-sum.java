abf29fc420ba76cbf62ab324efc791bbb19cf484
Muskan_Gupta <115883547+27082004muskan@users.noreply.github.com>
2025-02-12 15:28:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+
+//     int countPairs(int arr[], int target) {
+//       // Your code here
+//         int count =0;
+//         int n =arr.length;
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(arr[i]+arr[j]==target){
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count ;
+
+//     }
+// }
+
+    public int countPairs(int[] arr, int target) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+            int complement = target - num;
+            if (freq.containsKey(complement)) {
+                count += freq.get(complement); // Add occurrences of the complement
+            }
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int target = Integer.parseInt(read.readLine());
+
+            Solution obj = new Solution();
+
+            System.out.println(obj.countPairs(nums, target));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
